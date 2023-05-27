ff593b11280a77c149ab2669bf0d1f4db81927e4
samarth2002 <72395802+samarth2002@users.noreply.github.com>
2023-05-27 14:21:28 +0530

    Update factorial.cpp

--- Recursion/Practice/factorial.cpp
+++ Recursion/Practice/factorial.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
 
+using namespace std;
+
+int factorial(int n)
+{
+    if(n==0) return 1;
+
+    return n*factorial(n-1);
+    
+}
+
+int main(){
+    cout << factorial(3);
+    return 0;
+}
